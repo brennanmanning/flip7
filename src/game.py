f5d9f5898759fc68e480e0fbf689c52db70ea5db
Brennan Manning <manningb24@gmail.com>
2025-06-01 20:54:52 -0400

    Add defaults and logic to initialize new Game State

--- src/game.py
+++ src/game.py
@@ -2,19 +2,25 @@ from dataclasses import dataclass
 import dataclasses
 from typing import List, Union
 
-from card import Deck, add_cards, Card
-from player import Player
+from card import Deck, add_cards, Card, create_deck
+from player import Player, new_player
 
 
 @dataclass
 class GameState:
     players: List[Player]
     deck: Deck
-    discard_pile: Deck
-    current_player_idx: int
-    round_active: bool
+    discard_pile: Deck = Deck(tuple())
+    current_player_idx: int = 0
+    round_active: bool = True
     target_score: int = 200
 
 
-def add_card_discard(gs: GameState, card: Union[Card, Deck]):
+def add_card_discard(gs: GameState, card: Union[Card, Deck]) -> GameState:
     return dataclasses.replace(gs, discard_pile=add_cards(gs.discard_pile, card))
+
+
+def new_game(n_players: int = 2) -> GameState:
+    deck = create_deck()
+    players = [new_player() for _ in range(n_players)]
+    return GameState(players, deck)
