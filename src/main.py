9fc688afc49d160ee9388a31f4c3e892565283fc
Brennan Manning <manningb24@gmail.com>
2025-06-15 19:00:34 -0400

    remove unneeded code

--- src/main.py
+++ src/main.py
@@ -1,37 +1,5 @@
 from card import Deck, create_deck, draw_card, add_cards, value_hand
 
-# def turn(player: Player, gs: GameState):
-#     card, new_deck = draw_card(gs.deck)
-#     match card:
-#         case SpecialCard.CHANCE2:
-#             if check_duplicates(player, card):
-#                 print("give other player card")
-#             else:
-#                 player = add_card_hand(player, card)
-#                 return None
-#         case SpecialCard.FREEZE:
-#             print("give other player card or yourself")
-#         case SpecialCard.FLIP3:
-#             print("give other player card or yourself")
-#         case SpecialCard.X2:
-#             player = add_card_hand(player, card)
-#             return None
-#         case AdderCard():
-#             player = add_card_hand(player, card)
-#             return None
-#         case NumberCard():
-#             if check_duplicates(player, card):
-#                 if check_second_chance(player):
-#                     gs = add_card_discard(gs, card)
-#
-#                 gs = add_card_discard(gs, player.hand)
-#                 player = empty_hand(player)
-#                 player = freeze_player(player)
-#                 return None
-#             else:
-#                 player = add_card_hand(player, card)
-#
-
 
 def main():
     # TODO Think about how to deal with deck with cards in play
