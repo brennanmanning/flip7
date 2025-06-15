77a24f3a30e1edcd393bdf310f7a994adcfc266a
Brennan Manning <manningb24@gmail.com>
2025-06-15 19:01:54 -0400

    Add small util

--- src/utils.py
+++ src/utils.py
@@ -2,4 +2,12 @@ from typing import Tuple
 
 
 def pop_tuple(data: Tuple, idx: int) -> Tuple:
-    return data[:idx] + data[idx + 1:]
+    return data[:idx] + data[idx + 1 :]
+
+
+def bounded_add(val: int, bound: int, adder: int = 1):
+    val = val + adder
+    if val >= bound:
+        return 0
+    else:
+        return val
