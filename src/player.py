3437b426d136389eb00b896df2088a06fabad799
Brennan Manning <manningb24@gmail.com>
2025-06-15 19:01:34 -0400

    Add some player utilities

--- src/player.py
+++ src/player.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 import dataclasses
 
-from card import Card, Deck, add_cards
+from card import Card, Deck, add_cards, pop_deck, card_index, value_hand
 
 
 @dataclass(frozen=True)
@@ -23,9 +23,20 @@ def check_for_card(player: Player, card: Card) -> bool:
     return card in player.hand
 
 
-def add_card_hand(player: Player, card: Card):
+def add_card_hand(player: Player, card: Card) -> Player:
     return dataclasses.replace(player, hand=add_cards(player.hand, card))
 
 
-def empty_hand(player: Player):
+def empty_hand(player: Player) -> Player:
     return dataclasses.replace(player, hand=Deck(tuple()))
+
+
+def remove_card(player: Player, card: Card) -> Player:
+    card_idx = card_index(player.hand, card)
+    _, new_hand = pop_deck(player.hand, card_idx)
+    return dataclasses.replace(player, hand=new_hand)
+
+
+def update_score(player: Player):
+    hand_value = value_hand(player.hand)
+    return dataclasses.replace(player, score=hand_value + player.score)
